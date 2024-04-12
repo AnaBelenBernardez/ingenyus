8bf0896c6ce8c183cd4bbcc53d429c03d6554330
Ana <anabelenbernardez@gmail.com>
2024-04-12 13:33:58 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>ingenyus *</p>
+      </div>
+    </>
+  );
+}
+
+export default App;

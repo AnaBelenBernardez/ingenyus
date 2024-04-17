42b750cf4154f34749a11d46f921ba7bd2c26bfc
AyozeJesus <ayozejesusleonsantos@gmail.com>
2024-04-17 10:08:02 +0100

    change app.jsx and create pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>ingenyus *</h1>
+            <h2>Lastik</h2>
+            <h3>Satoshi</h3>
+            <p>Satoshi Italic</p>
+        </div>
+    );
+};
+
+export default Home;

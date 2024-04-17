623d6b05a60d8f46509f32abba55e8805b77348d
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-04-17 11:59:53 +0200

    contact & newsletter: figma measurement settings - UPDATED

--- src/assets/components/Contact&Newsletter/Contact&Newsletter.jsx
+++ src/assets/components/Contact&Newsletter/Contact&Newsletter.jsx
@@ -8,16 +8,16 @@ export function Contact_Newsletter() {
         <section className="newsletter_contact">
             <div className="contact">
                 <h4 className="both_title">contacto</h4>
-                <p className="nc_text">urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed</p>
-                <p className="nc_text">nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem</p>
-            </div>
-            <div className="contact_button_box">
-                <BotonSimple clase={"button_contact"} children={"contacto"} />
+                <p className="nc_text">Resumir en una web todo lo que hacemos es complicado. Te invitamos a que nos cuentes sobre tu proyecto.</p>
+                <p className="nc_text">Nuestro equipo se reunirá contigo para valorar conjuntamente en qué podemos ayudarte. Nos podemos reunir por videollamada, en un café o en nuestras oficinas en Calle Real 24, Planta 1ª, 15003, A Coruña.</p>
+                <div className="contact_button_box">
+                    <BotonSimple clase={"button_contact"} children={"contacto"} />
+                </div>
             </div>
             <div className="newsletter">
                 <div className="newsletter_left">
                     <h4 className="both_title">newsletter</h4>
-                    <p className="nc_text">urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed</p>
+                    <p className="nc_text">¡Suscríbete! Prometemos no enviar demasiadas, y sólo con información de interés como ofertas específicas, financiación…</p>
                 </div>
                 <div className="newsletter_right">
                     <form className="newsletter_email" action="">

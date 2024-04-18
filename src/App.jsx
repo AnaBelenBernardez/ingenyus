fad93f60c2cff212310cbc3d6428ca7492934d6a
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-04-18 12:17:55 +0200

    media query: Established 3 sizes (can be changed)

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Chemistry from './pages/Chemistry';
 import Mathematics from './pages/Mathematics';
 import ComputerScience from './pages/ComputerScience';
 import Medicine from './pages/Medicine';
-import './App.css';
+import '../src/css/base/_globals.css';
 import { Contact_Newsletter } from './components/Contact&Newsletter/Contact&Newsletter';
 import Header from './components/Header';
 import PageTitle from './components/PageTitle';
 
 function App() {
     return (
-        <div className='App'>
+        <>
             <PageTitle />
             <Header />
             <Routes>
@@ -26,7 +26,8 @@ function App() {
                 <Route path='medicine' element={<Medicine />} />
                 <Route path='*' element={<div>404 Not Found</div>} />
             </Routes>
-        </div>
+            <Contact_Newsletter />
+        </>
     );
 }
 

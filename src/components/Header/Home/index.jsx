a69eb53330566e7d4e42616cd204cebed9a29fcb
Ana <anabelenbernardez@gmail.com>
2024-04-19 14:29:09 +0200

    responsive logo

--- src/components/Header/Home/index.jsx
+++ src/components/Header/Home/index.jsx
@@ -1,12 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './style.css';
 
 function Home() {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1200);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 1200);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className='home'>
             <button className='buttonHome'>
                 <Link id='linkHome' to='/'>
-                    ingenyus*
+                    <img
+                        className='logoButtonHome'
+                        src={
+                            isDesktop
+                                ? '/logos/in_logo_desktop.svg'
+                                : '/logos/in_logo_responsive.svg'
+                        }
+                        alt='ingenyus*'
+                    />
                 </Link>
             </button>
         </div>

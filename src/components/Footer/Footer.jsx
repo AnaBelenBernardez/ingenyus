48cbf4b7b2bff50f4bf0f3adb7083e16377ed2ff
lucia aguirre <guerrolucia@gmail.com>
2024-04-22 10:37:36 +0200

    adds basic footer css pending

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import camaracomercio from '/logos/camaracomercio.png';
+import cluster from '/logos/ClusterdaComunicacionlogoclusterdecomunicacion.png';
+import groupclusaga from '/logos/groupclusaga.png';
+import logoasebio from '/logos/LogoAsebioNombrecompletoTRANSPARENTE.png';
+import viratec from '/logos/VIRATEC.png';
+
+function Footer() {
+    return (
+        <footer>
+            <section>
+                <p>unusual agency</p>
+
+                <p> hola@ingenyus.es</p>
+
+                <p>únete al equipo talent@ingenyus.es</p>
+
+                <p>
+                    +34 981 227 225 +34 968 107 558 R.Real,24, 1º, 15003 A
+                    Coruña
+                </p>
+
+                <a href='linkedin'> Linkedin</a>
+                <a href='twitter'> Twitter</a>
+                <a href='instagram'> Instagram</a>
+                <a href='T'>T</a>
+
+                <p>Kit digital</p>
+            </section>
+            <section>
+                <img src={camaracomercio} alt='Logo de la cámara de comercio' />
+                <img
+                    src={cluster}
+                    alt='Logo Cluster de Comunicacion de Galicia'
+                />
+                <img
+                    src={groupclusaga}
+                    alt='Logo Group Cluster Alimentario de Galicia'
+                />
+                <img src={logoasebio} alt='Logo ASEBIO' />
+                <img src={viratec} alt='Logo Viratec' />
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;

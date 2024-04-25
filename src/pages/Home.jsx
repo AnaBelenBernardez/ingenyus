eafe0eff3256f7d6bfc90bad97ff9d9d03fa194c
Ana <anabelenbernardez@gmail.com>
2024-04-25 18:24:16 +0200

    HomePage styles for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,29 +7,43 @@ const Home = () => {
 
     return (
         <main className='noise homePage'>
-            <h1 className='homeTitle'>
-                <span id='homeTitle1'> {t('translation.title')}</span>
-                <span id='homeTitle2'> {t('translation.title2')}</span>
-                <span id='homeTitle3'> {t('translation.title3')}</span>
-            </h1>
-            <img className='homeAddons' src='./logos/HAB_icon.svg' alt='' />
-            <img className='homeAddons' src='./logos/in_asterisco.svg' alt='' />
-            <span className='colab'>ingenyus x hack a boss</span>
-            <div className='homeScrollContainer'>
-                <span>scroll</span>
-                <div className='homeScroll'>
-                    <svg
-                        width='14'
-                        height='9'
-                        viewBox='0 0 14 9'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
-                        <path
-                            d='M6.6678 8.14006L0.0512695 1.52353L0.699949 0.874847L6.6678 6.8427L12.6357 0.874847L13.2843 1.52353L6.6678 8.14006Z'
-                            fill='white'
+            <div className='landingHome'>
+                <h1 className='homeTitle'>
+                    <span id='homeTitle1'> {t('translation.title')}</span>
+                    <span id='homeTitle2'>
+                        <img
+                            id='homeHABAddon'
+                            src='./logos/HAB_icon.svg'
+                            alt=''
                         />
-                    </svg>
+                        {t('translation.title2')}
+                    </span>
+                    <span id='homeTitle3'>
+                        {t('translation.title3')}
+                        <img
+                            id='homeINAddon'
+                            src='./logos/in_asterisco.svg'
+                            alt=''
+                        />
+                    </span>
+                </h1>
+                <span className='colab'>ingenyus x hack a boss</span>
+                <div className='homeScrollContainer'>
+                    <span>scroll</span>
+                    <div className='homeScroll'>
+                        <svg
+                            width='14'
+                            height='9'
+                            viewBox='0 0 14 9'
+                            fill='none'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                d='M6.6678 8.14006L0.0512695 1.52353L0.699949 0.874847L6.6678 6.8427L12.6357 0.874847L13.2843 1.52353L6.6678 8.14006Z'
+                                fill='white'
+                            />
+                        </svg>
+                    </div>
                 </div>
             </div>
             <nav className='homeNav'>

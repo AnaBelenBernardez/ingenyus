517e6de37ee6e44bd1e80e8177eb75d3cf3761ca
AyozeJesus <ayozejesusleonsantos@gmail.com>
2024-04-26 18:03:03 +0100

    Implement loading indicators on all pages.

--- src/pages/Biology.jsx
+++ src/pages/Biology.jsx
@@ -1,7 +1,40 @@
 import BiologyMap from '../components/Maps/BiologyMap';
 import '../css/layout/_General.css';
 import '../css/layout/_Maps.css';
+import Loading from '../components/Loading/index.jsx';
+import { useEffect, useState } from 'react';
+
 const Biology = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [showLoading, setShowLoading] = useState(false);
+
+    useEffect(() => {
+        let showLoadingTimeout = setTimeout(() => {
+            setShowLoading(true);
+        }, 100);
+
+        fetch('../assets/data/data.json')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                clearTimeout(showLoadingTimeout);
+                setIsLoading(false);
+                setShowLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+                setIsLoading(false);
+                setShowLoading(false);
+            });
+
+        return () => {
+            clearTimeout(showLoadingTimeout);
+        };
+    }, []);
+
+    if (isLoading && showLoading) {
+        return <Loading />;
+    }
     return <BiologyMap />;
 };
 
--- src/pages/Physics.jsx
+++ src/pages/Physics.jsx
@@ -1,7 +1,39 @@
 import PhysicsMap from '../components/Maps/PhysicsMap';
 import '../css/layout/_General.css';
+import Loading from '../components/Loading/index.jsx';
+import { useEffect, useState } from 'react';
 
 const Physics = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [showLoading, setShowLoading] = useState(false);
+
+    useEffect(() => {
+        let showLoadingTimeout = setTimeout(() => {
+            setShowLoading(true);
+        }, 100);
+
+        fetch('../assets/data/data.json')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                clearTimeout(showLoadingTimeout);
+                setIsLoading(false);
+                setShowLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+                setIsLoading(false);
+                setShowLoading(false);
+            });
+
+        return () => {
+            clearTimeout(showLoadingTimeout);
+        };
+    }, []);
+
+    if (isLoading && showLoading) {
+        return <Loading />;
+    }
     return <PhysicsMap />;
 };
 

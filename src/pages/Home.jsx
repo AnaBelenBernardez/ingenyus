d627c9101d76b6aac01aa93d254ea6392261ea51
Ana <anabelenbernardez@gmail.com>
2024-05-07 20:24:49 +0200

    Background to PC

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,54 +57,61 @@ const Home = () => {
     }
 
     return (
-        <main className='noise homePage blurBack'>
-            <div className='ellipseWhite' id='e8'></div>
+        <>
+            {/* <div className='ellipseWhite' id='e8'></div>
             <div className='ellipseBlue' id='e7'></div>
-            <div className='ellipseWhite' id='e11'></div>
-            <div className='ellipseWhite' id='e9'></div>
             <div className='ellipseBlue' id='e10'></div>
-            <div className='landingHome'>
-                <h1 className='homeTitle'>
-                    <span id='homeTitle1'> {t('translation.title')}</span>
-                    <span id='homeTitle2'>
-                        <img
-                            id='homeHABAddon'
-                            src='./logos/HAB_icon.svg'
-                            alt=''
-                        />
-                        {t('translation.title2')}
-                    </span>
-                    <span id='homeTitle3'>
-                        {t('translation.title3')}
-                        <img
-                            id='homeINAddon'
-                            className={isEnglish ? 'englishAddon' : ''}
-                            src='./logos/in_asterisco.svg'
-                            alt=''
-                        />
-                    </span>
-                </h1>
-                <span className='colab'>ingenyus x hack a boss</span>
-                <div className='homeScrollContainer'>
-                    <span>scroll</span>
-                    <div className='homeScroll'>
-                        <svg
-                            width='14'
-                            height='9'
-                            viewBox='0 0 14 9'
-                            fill='none'
-                            xmlns='http://www.w3.org/2000/svg'
-                        >
-                            <path
-                                d='M6.6678 8.14006L0.0512695 1.52353L0.699949 0.874847L6.6678 6.8427L12.6357 0.874847L13.2843 1.52353L6.6678 8.14006Z'
-                                fill='var(--athens)'
+            <div className='ellipseWhite' id='e9'></div>
+            <div className='ellipseWhite' id='e11'></div> */}
+            <main className='noise homePage blurBack'>
+                <div className='ellipseWhite' id='e8'></div>
+                <div className='ellipseBlue' id='e7'></div>
+                <div className='ellipseBlue' id='e10'></div>
+                <div className='ellipseWhite' id='e9'></div>
+                <div className='ellipseWhite' id='e11'></div>
+                <div className='landingHome'>
+                    <h1 className='homeTitle'>
+                        <span id='homeTitle1'> {t('translation.title')}</span>
+                        <span id='homeTitle2'>
+                            <img
+                                id='homeHABAddon'
+                                src='./logos/HAB_icon.svg'
+                                alt=''
                             />
-                        </svg>
+                            {t('translation.title2')}
+                        </span>
+                        <span id='homeTitle3'>
+                            {t('translation.title3')}
+                            <img
+                                id='homeINAddon'
+                                className={isEnglish ? 'englishAddon' : ''}
+                                src='./logos/in_asterisco.svg'
+                                alt=''
+                            />
+                        </span>
+                    </h1>
+                    <span className='colab'>ingenyus x hack a boss</span>
+                    <div className='homeScrollContainer'>
+                        <span>scroll</span>
+                        <div className='homeScroll'>
+                            <svg
+                                width='14'
+                                height='9'
+                                viewBox='0 0 14 9'
+                                fill='none'
+                                xmlns='http://www.w3.org/2000/svg'
+                            >
+                                <path
+                                    d='M6.6678 8.14006L0.0512695 1.52353L0.699949 0.874847L6.6678 6.8427L12.6357 0.874847L13.2843 1.52353L6.6678 8.14006Z'
+                                    fill='var(--athens)'
+                                />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {isDesktop ? <DesktopNavHome /> : <MobileNavHome />}
-        </main>
+                {isDesktop ? <DesktopNavHome /> : <MobileNavHome />}
+            </main>
+        </>
     );
 };
 

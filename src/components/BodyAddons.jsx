97686e42c3d6e4aba4e12dba1323cd27f7818f21
Ana <anabelenbernardez@gmail.com>
2024-05-13 21:02:07 +0200

    accesibility fixes

--- src/components/BodyAddons.jsx
+++ src/components/BodyAddons.jsx
@@ -7,13 +7,13 @@ const BodyAddons = () => {
                 src='../../logos/in_asterisco.svg'
                 className='bodyAddons'
                 id='bodyIN'
-                alt='Icono de Ingenyus'
+                alt=''
             />
             <img
                 src='../../logos/HAB_icon.svg'
                 className='bodyAddons'
                 id='bodyHAB'
-                alt='Icono de HAB'
+                alt=''
             />
             <div id='colabBody'>
                 <span className='colab'>ingenyus x hack a boss</span>

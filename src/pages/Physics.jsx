b40bd20ede9b6062e719b5e9d51cb762d9ff3169
Ana Belén Bernárdez Martínez <anabelenbernardez@gmail.com>
2024-05-14 19:01:25 +0200

    fixing component logic

--- src/pages/Physics.jsx
+++ src/pages/Physics.jsx
@@ -53,27 +53,25 @@ const Physics = () => {
         return <Loading />;
     }
     return (
-        <main className='mainMap blurBack'>
+        <main className='mainMap blurBack backgroundSolid'>
             <div className='noiseBack'> </div>
-            <div className='backgroundSolid'>
-                <PhysicsMap />
-                <div className='ellipseWhiteMap' id='e22'></div>
-                <div className='ellipseWhiteMap' id='e18'></div>
-                <div className='ellipseWhiteMap' id='e23'></div>
-                <div className='ellipseWhiteMap' id='e19'></div>
-                <div className='ellipseWhiteMap' id='e12'></div>
-                <div className='ellipseWhiteMap' id='e14'></div>
-                <div className='ellipseWhiteMap' id='e21'></div>
-                <div className='ellipseWhiteMap' id='e15'></div>
-                <div className='ellipseBlueMap' id='e24'></div>
-                <div className='ellipseBlueMap' id='e25'></div>
-                <div className='ellipseBlueMap' id='e13'></div>
-                <div className='ellipseBlueMap' id='e20'></div>
-                <div className='ellipseBlueMap' id='e17'></div>
-                <div className='ellipseBlueMap' id='e16'></div>
-                {isDesktop ? <DesktopNavHome /> : <MobileNavHome />}
-                <BodyAddons />
-            </div>
+            <PhysicsMap />
+            <div className='ellipseWhiteMap' id='e22'></div>
+            <div className='ellipseWhiteMap' id='e18'></div>
+            <div className='ellipseWhiteMap' id='e23'></div>
+            <div className='ellipseWhiteMap' id='e19'></div>
+            <div className='ellipseWhiteMap' id='e12'></div>
+            <div className='ellipseWhiteMap' id='e14'></div>
+            <div className='ellipseWhiteMap' id='e21'></div>
+            <div className='ellipseWhiteMap' id='e15'></div>
+            <div className='ellipseBlueMap' id='e24'></div>
+            <div className='ellipseBlueMap' id='e25'></div>
+            <div className='ellipseBlueMap' id='e13'></div>
+            <div className='ellipseBlueMap' id='e20'></div>
+            <div className='ellipseBlueMap' id='e17'></div>
+            <div className='ellipseBlueMap' id='e16'></div>
+            {isDesktop ? <DesktopNavHome /> : <MobileNavHome />}
+            <BodyAddons />
         </main>
     );
 };

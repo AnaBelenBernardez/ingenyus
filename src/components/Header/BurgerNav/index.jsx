07a2e325c9397b9faa09e5279c82b97a36331141
Ana Belén Bernárdez Martínez <anabelenbernardez@gmail.com>
2024-05-15 13:12:29 +0200

    merge fixed

--- src/components/Header/BurgerNav/index.jsx
+++ src/components/Header/BurgerNav/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { slide as BurgerMenu } from 'react-burger-menu';
+import { useState } from 'react';
 import './style.css';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -15,20 +14,18 @@ const BurgerNav = () => {
 
     const handleItemClick = () => {
         setIsOpen(false);
-        setIsOpen(false); // Cerramos el menú primero
-        window.scrollTo(0, 0); // Luego hacemos scroll hacia arriba
+        window.scrollTo(0, 0);
     };
-
-    };
-
-
 
     return (
         <>
-
-            <div className={`botaoHamburguer ${isOpen ? 'open' : ''}`} id="nav-icon4" onClick={handleMenuToggle}>
+            <div
+                className={`botaoHamburguer ${isOpen ? 'open' : ''}`}
+                id='nav-icon4'
+                onClick={handleMenuToggle}
+            >
                 <span></span>
-                <span className="hoverEffect"></span>
+                <span className='hoverEffect'></span>
                 <span></span>
             </div>
             <nav className={`navBurger ${isOpen ? 'open' : 'close'}`}>
@@ -85,72 +82,7 @@ const BurgerNav = () => {
                     <LanguageSelector />
                 </ul>
             </nav>
-
         </>
-
-
-        // <BurgerMenu
-        //     right
-        //     width={'100%'}
-        //     isOpen={isOpen}
-        //     className='burgerMenu'
-        //     onStateChange={(state) => setIsOpen(state.isOpen)}
-        // >
-        //     <nav className='navBurger'>
-        //         <ul>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink exact='true' to='/'>
-        //                         home
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='chemistry'>
-        //                         {t('translation.chemistry')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='physics'>
-        //                         {t('translation.physics')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='biology'>
-        //                         {t('translation.biology')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='computer-science'>
-        //                         {t('translation.computerScience')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='mathematics'>
-        //                         {t('translation.mathematics')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <li>
-        //                 <button className='buttonNav' onClick={handleItemClick}>
-        //                     <NavLink to='medicine'>
-        //                         {t('translation.medicine')}
-        //                     </NavLink>
-        //                 </button>
-        //             </li>
-        //             <LanguageSelector />
-        //         </ul>
-        //     </nav>
-        // </BurgerMenu>
     );
 };
 

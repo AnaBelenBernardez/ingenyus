11eab07ada1916afbcc2d0a622f9fddbfef768fb
Ana Belén Bernárdez Martínez <anabelenbernardez@gmail.com>
2024-06-04 18:37:02 +0200

    Fix: Safari CSS

--- src/pages/Biology.jsx
+++ src/pages/Biology.jsx
@@ -8,7 +8,7 @@ import MobileNavHome from '../components/MobileNavHome.jsx';
 import BodyAddons from '../components/BodyAddons.jsx';
 import '../css/base/_backgrounds.css';
 import Header from '../components/Header/index.jsx';
-import { Contact_Newsletter } from '../components/Contact&Newsletter/Contact&Newsletter.jsx';
+import { Contact_Newsletter } from '../components/ContactNewsletter/ContactNewsletter.jsx';
 import Footer from '../components/Footer/Footer.jsx';
 
 const Biology = () => {
